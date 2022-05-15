125a9169133ff79bb6eab5a09b413dc2d2cc604b
wayer.ne@gmail.com <wayerne>
2022-05-15 14:18:46 +0500

    Updates

---
+++ precache-manifest.5c06d349031c70b13acad79438ef137f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "826bcfcf4a782d6e7202",
+    "url": "/static/css/main.5a51930f.chunk.css"
+  },
+  {
+    "revision": "826bcfcf4a782d6e7202",
+    "url": "/static/js/main.826bcfcf.chunk.js"
+  },
+  {
+    "revision": "f26d710076045c535df7",
+    "url": "/static/css/1.df741a24.chunk.css"
+  },
+  {
+    "revision": "f26d710076045c535df7",
+    "url": "/static/js/1.f26d7100.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9b3ce97ef5497ae1c2078417fa918893",
+    "url": "/static/media/home.9b3ce97e.jpg"
+  },
+  {
+    "revision": "49afadd172d50860cfcd07f4e26b74a1",
+    "url": "/static/media/favicon_for_iumovie.49afadd1.ico"
+  },
+  {
+    "revision": "b57a58905370284f1499d1a35f5d2c96",
+    "url": "/static/media/bootstrap-icons.b57a5890.woff2"
+  },
+  {
+    "revision": "a6a4862c8f0a585c6c1f6d5aa07b761c",
+    "url": "/static/media/bootstrap-icons.a6a4862c.woff"
+  },
+  {
+    "revision": "7e21cd11591f62a1141f11551619c5da",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/iumovie/precache-manifest.e924eadaf031bc1c86555914e44163b4.js"
+  "/precache-manifest.5c06d349031c70b13acad79438ef137f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/iumovie/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
